7d9035009b8554bdbfe40ef47b15b606cc5581bb
Stefan Haustein <stefanhaustein@Mini-von-Stefan.home>
2022-02-14 20:25:48 +0100

    cleanup

--- shared/src/commonMain/kotlin/org/kobjects/kxml3/KXmlFactory.kt
+++ shared/src/commonMain/kotlin/org/kobjects/kxml3/KXmlFactory.kt
@@ -7,7 +7,7 @@ import org.xmlpull.v2.XmlPullFactory
 class KXmlFactory(
     override var validating: Boolean = false,
     override var namespaceAware: Boolean = false,
-    override var entityResolver: (String) -> String? = { it -> null },
+    override var entityResolver: (String) -> String? = { null },
     override var relaxed: Boolean = false
 ) : XmlPullFactory {
     override fun createParser(source: CharIterator) = KXmlParser(
