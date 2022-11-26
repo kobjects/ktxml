80dd618bda8e3e864e37037f9f95c85fd66b5133
Stefan Haustein <stefan.haustein@gmail.com>
2022-11-26 20:26:13 +0100

    ?

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -12,7 +12,7 @@ version = "0.2.2"
 kotlin {
     iosX64()
     iosArm64()
-    //iosSimulatorArm64() sure all ios dependencies support this target
+    iosSimulatorArm64()
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
@@ -43,21 +43,21 @@ kotlin {
 
         val iosX64Main by getting
         val iosArm64Main by getting
-        //val iosSimulatorArm64Main by getting
+        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            //iosSimulatorArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        //val iosSimulatorArm64Test by getting
+        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            //iosSimulatorArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
 
         val jvmMain by getting
@@ -68,7 +68,11 @@ kotlin {
         }
 
         val jsMain by getting
-        val jsTest by getting
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
 
     }
 }
