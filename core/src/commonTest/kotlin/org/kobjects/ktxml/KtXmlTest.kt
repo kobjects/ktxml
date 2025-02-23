55897b3e4a2c97dd55d27b377dc26bebb8b7452e
Stefan Haustein <stefan.haustein@gmail.com>
2025-02-23 22:20:49 +0100

    Add a test case for the re-incarnation of issue #5 and give in to the ide nagging for a gradle update

--- core/src/commonTest/kotlin/org/kobjects/ktxml/KtXmlTest.kt
+++ core/src/commonTest/kotlin/org/kobjects/ktxml/KtXmlTest.kt
@@ -47,4 +47,21 @@ class KtXmlTest {
         assertEquals(EventType.END_TAG, parser.nextToken())
         assertEquals(EventType.END_DOCUMENT, parser.nextToken())
     }
+
+    @Test
+    fun testXmlDecl2() {
+        val parser = MiniXmlPullParser("""<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/" />
+""".iterator())
+        assertEquals(EventType.START_DOCUMENT, parser.eventType)
+        assertEquals(EventType.XML_DECL, parser.nextToken())
+        assertEquals(EventType.START_TAG, parser.nextToken())
+        assertEquals(EventType.END_TAG, parser.nextToken())
+        assertEquals(EventType.END_DOCUMENT, parser.nextToken())
+    }
 }
